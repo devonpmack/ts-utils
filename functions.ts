f24bf063398808c6826736dc797d6ab96a6f986c
Devon Mack <devon.mack@shopify.com>
2022-05-17 15:00:07 -0400

    separate into types and functions

---
+++ functions.ts
@@ -0,0 +1,14 @@
+/**
+ * Typed version of Object.keys, returning strongly typed keys
+ *
+ * @example
+ * ```typescript
+ * const keys = keysOf({ a: 1, b: 2 }); // ["a", "b"]
+ * typeof keys[0]; // "a" | "b"
+ * ```
+ */
+export function keysOf<T, TKeys extends Extract<keyof T, string>[]>(
+  obj: T
+): TKeys {
+  return Object.keys(obj) as TKeys;
+}
